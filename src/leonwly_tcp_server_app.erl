48ef6fd32994ee346f8dccade2dcfa90882a6907
leonwly <leonwang.beyond@gmail.com>
2016-08-04 21:28:30 +0800

    first version, empty code framework
    This is first version code.

---
+++ src/leonwly_tcp_server_app.erl
@@ -0,0 +1,16 @@
+-module(leonwly_tcp_server_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    leonwly_tcp_server_sup:start_link().
+
+stop(_State) ->
+    ok.
